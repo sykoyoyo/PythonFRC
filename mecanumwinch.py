bc6a16b7fa4246b2335a21a51b5de4af8028d7f1
Joshua <bb20basketball@gmail.com>
2017-09-19 08:30:12 -0500

    Changed from sample robot to iternative

--- mecanumwinch.py
+++ mecanumwinch.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
-'''
-    This is a demo program showing how to use Mecanum control with the
-    RobotDrive class.
-    '''
 
 import wpilib
-from wpilib import RobotDrive
+#!/usr/bin/env python3
 
-class MyRobot(wpilib.SampleRobot):
-    
+import wpilib # <---- main library with all the necessary code for controlling FRC robots
+import wpilib.buttons # <---- for joystick buttons
+
+class MyRobot(wpilib.IterativeRobot): # <---- IternativeRobot means that it loops over and over by itself like a while loop
+
     # Channels for the wheels
     frontLeftChannel    = 2
     rearLeftChannel     = 3
@@ -19,51 +18,55 @@ class MyRobot(wpilib.SampleRobot):
     winchMotor2        = 5
     
     # The channel on the driver station that the joystick is connected to
-    joystickChannel     = 0;
+    joystickChannel     = 0
     
     def robotInit(self):
-        '''Robot initialization function'''
+
+        """
+            Define all motor controllers, joysticks, Pneumatics, etc. here so you can use them in teleop/auton
+        """
 
         self.robotDrive = wpilib.RobotDrive(self.frontLeftChannel,
                                             self.rearLeftChannel,
                                             self.frontRightChannel,
                                             self.rearRightChannel)
-            
-        self.robotDrive.setExpiration(0.1)
 
-        self.robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, True)
+        self.robotDrive.setInvertedMotor(wpilib.RobotDrive.MotorType.kFrontLeft, True)
 
-        self.robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, True)
-
+        self.robotDrive.setInvertedMotor(wpilib.RobotDrive.MotorType.kRearLeft, True)
+        
         self.winch_motor2 = wpilib.Talon(self.winchMotor2)
         self.winch_motor1 = wpilib.Talon(self.winchMotor1)
         
         self.stick = wpilib.Joystick(self.joystickChannel)
-    
-    def operatorControl(self):
-        '''Runs the motors with Mecanum drive.'''
-        
-        self.robotDrive.setSafetyEnabled(True)
-        while self.isOperatorControl() and self.isEnabled():
-            
 
-            self.robotDrive.mecanumDrive_Cartesian(self.stick.getX(),
+
+    def teleopInit(self):
+
+        """
+            Put code in here that you want ran once before teleop starts.
+            I use it for timers and counters.
+        """
+        pass
+
+    def teleopPeriodic(self):
+
+        """
+            Human controlled period
+        """
+        self.robotDrive.mecanumDrive_Cartesian(self.stick.getX(),
                                                    self.stick.getY(),
-                                                   self.stick.getZ(), 0);
+                                                   self.stick.getRawAxis(5), 0);
                                                    
-            if self.stick.getRawButton(3):
-                self.winch_motor2.set(1)
-                self.winch_motor1.set(1)
-            elif self.stick.getRawButton(4):
-                self.winch_motor1.set(-1)
-                self.winch_motor2.set(-1)
-            else:
-                self.winch_motor1.set(0)
-                self.winch_motor2.set(0)
-                                                   
-            wpilib.Timer.delay(0.005)
-
-
-
-if __name__ == '__main__':
-    wpilib.run(MyRobot)
+        if self.stick.getRawButton(3):
+            self.winch_motor2.set(1)
+            self.winch_motor1.set(1)
+        elif self.stick.getRawButton(4):
+            self.winch_motor1.set(-1)
+            self.winch_motor2.set(-1)
+        else:
+            self.winch_motor1.set(0)
+            self.winch_motor2.set(0)
+        
+if __name__ == "__main__":
+    wpilib.run(MyRobot) #runs the code!
