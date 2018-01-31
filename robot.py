886c857ea3e213e06d8f035b3b7d27088bc9beb8
Mr. Robby <crobertson@upsala.k12.mn.us>
2018-01-30 18:12:26 -0600

    2018 initial code
    RoboRio Calls, Arcade Drive Controls, 2 joysticks, motor intakes, motor shoot for out rear

--- robot.py
+++ robot.py
@@ -1,120 +1,118 @@
 #!/usr/bin/env python3
 
+import wpilib # <---- main library with all the necessary code for controlling FRC robots
+import wpilib.buttons # <---- for joystick buttons
 
-import wpilib
-import wpilib.buttons
-from wpilib import RobotDrive
-from robotpy_ext.common_drivers import navx
+class MyRobot(wpilib.IterativeRobot): # <---- IternativeRobot means that it loops over and over by itself like a while loop
 
-
-class MyRobot(wpilib.IterativeRobot):
-    
-    '''Insert early definitions for Channels of Speed controls'''
-    
     # Channels for the wheels
-    frontLeftChannel    = 3
-    rearLeftChannel     = 2
-    frontRightChannel   = 4
-    rearRightChannel    = 1
+    topLeftChannel      = 1
+    bottomLeftChannel   = 2
+    topRightChannel     = 3
+    bottomRightChannel  = 4
     
-    winchMotor1        = 7
-    winchMotor2        = 8
+    leftIntakeMotor     = 5
+    rightIntakeMotor    = 6
+
+    sweeperMotor         = 11
+
+    stage2LeftMotor     = 7
+    stage2RightMotor    = 8
+
+    stage3LeftMotor     = 9
+    stage3RightMotor    = 10
     
     # The channel on the driver station that the joystick is connected to
-    joystickChannel     = 0
+    joystickChannelDrive    = 1
+    joystickChannelShoot    = 2
+
     
     def robotInit(self):
-        '''Robot initialization function - Define your inputs, and what channels they connect to'''
-        
-        #Which Channels are plugged into which Motor Controls for Mecanum
-        #also Hack to see on SIM
-        
-        if not wpilib.RobotBase.isSimulation():
-            import ctre
-        
-            self.BRC = ctre.CANTalon(self.rearRightChannel)
-            self.BLC = ctre.CANTalon(self.rearLeftChannel)
-            self.FLC = ctre.CANTalon(self.frontLeftChannel)
-            self.FRC = ctre.CANTalon(self.frontRightChannel)
 
-        else:
-            self.BRC = wpilib.Talon(self.rearRightChannel)
-            self.BLC = wpilib.Talon(self.rearLeftChannel)
-            self.FLC = wpilib.Talon(self.frontLeftChannel)
-            self.FRC = wpilib.Talon(self.frontRightChannel)
-        
-        #Defining Mecanum channels
+        """
+            Define all motor controllers, joysticks, Pneumatics, etc. here so you can use them in teleop/auton
+        """
 
-        self.robotDrive = wpilib.RobotDrive(self.FLC,
-                                            self.BLC,
-                                            self.FRC,
-                                            self.BRC)
-            
-        self.robotDrive.setExpiration(0.1)
+        self.robotDrive = wpilib.RobotDrive(self.topLeftChannel,
+                                            self.bottomLeftChannel,
+                                            self.topRightChannel,
+                                            self.bottomRightChannel)
         
-        #Invert two Drive Motors for Mecanum
+        self.stick = wpilib.Joystick(self.joystickChannelDrive)
+        self.shooter = wpilib.Joystick(self.joystickChannelShoot)
         
-        self.robotDrive.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, True)
+        self.intakeLeft = wpilib.Talon(self.leftIntakeMotor)
+        self.intakeRight = wpilib.Talon(self.rightIntakeMotor)
 
-        self.robotDrive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, True)
-        
-        #Winch Motor Controls
-        
-        self.winch_motor2 = wpilib.Talon(self.winchMotor2)
-        self.winch_motor1 = wpilib.Talon(self.winchMotor1)
-        
-        #Joystick Definition
-        
-        self.stick = wpilib.Joystick(self.joystickChannel)
-        
-        #Pneumatics Code
-    
-        self.fire_single_piston = wpilib.buttons.JoystickButton(self.stick, 3)
-        self.fire_double_forward = wpilib.buttons.JoystickButton(self.stick, 2)
-        self.fire_double_backward = wpilib.buttons.JoystickButton(self.stick, 1)
-    
-        self.single_solenoid = wpilib.Solenoid(1)
-        self.double_solenoid = wpilib.DoubleSolenoid(2,3)
-    
+        self.stage2Left = wpilib.Talon(self.stage2LeftMotor)
+        self.stage2Right = wpilib.Talon(self.stage2RightMotor)
+
+        self.stage3Left = wpilib.Talon(self.stage3LeftMotor)
+        self.stage3Right = wpilib.Talon(self.stage3RightMotor)
+
+        self.sweeper = wpilib.Talon(self.sweeperMotor)
 
 
-    
+        self.shifterForward = wpilib.buttons.JoystickButton(self.stick, 2) # Xbox controller button Number 2 (B)
+        self.shifterBack = wpilib.buttons.JoystickButton(self.stick, 3) # Xbox controller button Number 3 (X)
+
+        self.double_solenoid_piston = wpilib.DoubleSolenoid(2,3) # Double Solenoid on port 2 and 3
+
     def teleopInit(self):
-        ''' runs Sensors and timers etc'''
-        
+
+        """
+            Put code in here that you want ran once before teleop starts.
+            I use it for timers and counters.
+        """
         pass
-    
+
     def teleopPeriodic(self):
-        '''Runs the motors, Button controls, solenoids etc'''
-        
+
+        """
+            Human controlled period
+        """
+#Make the robot drive there now
+        self.robotDrive.arcadeDrive(self.stick, True)
             
-        #Drive Command
-        self.robotDrive.mecanumDrive_Cartesian(self.stick.getRawAxis(4), self.stick.getY(), self.stick.getX(), 0)
-        
-        #Winch Buttons
-        if self.stick.getRawButton(5):
-                self.winch_motor2.set(1)
-                self.winch_motor1.set(1)
-        elif self.stick.getRawButton(6):
-                self.winch_motor1.set(-1)
-                self.winch_motor2.set(-1)
+#That there Intake looks not too shabby 
+        if self.shooter.getRawButton(3):
+            self.intakeLeft.set(1)
+            self.intakeRight.set(-1)
+            self.stage2Left.set(1)
+            self.stage2Right.set(-1)
+            
+        elif self.shooter.getRawButton(4):
+            self.intakeRight.set(1)
+            self.intakeLeft.set(-1)
+            self.stage2Right.set(1)
+            self.stage2Left.set(-1)
+            
         else:
-                self.winch_motor1.set(0)
-                self.winch_motor2.set(0)
-        
-        #Flipper for gear intake
-        if (self.fire_single_piston.get()):
-            self.single_solenoid.set(True)
+            self.intakeLeft.set(0)
+            self.intakeRight.set(0)
+            self.stage2Left.set(0)
+            self.stage2Right.set(0)
+            
+#OH SHOOT DER
+        if self.shooter.getRawAxis(3):
+            self.stage3Left.set(1)
+            self.stage3Right.set(-1)
+        elif self.shooter.getRawAxis(6):
+            self.stage3Right.set(1)
+            self.stage3Left.set(-1)
         else:
-            self.single_solenoid.set(False)
-        
-        #Open/Close Gear placer
-        if (self.fire_double_forward.get()):
-            self.double_solenoid.set(wpilib.DoubleSolenoid.Value.kForward)
+            self.stage3Left.set(0)
+            self.stage3Right.set(0)
 
-        elif (self.fire_double_backward.get()):
-            self.double_solenoid.set(wpilib.DoubleSolenoid.Value.kReverse)
+#Holy Shift Batman
+            
+        if (self.shifterForward.get()):
+            self.double_solenoid_piston.set(wpilib.DoubleSolenoid.Value.kForward)
+
+        elif (self.shifterBack.get()):
+            self.double_solenoid_piston.set(wpilib.DoubleSolenoid.Value.kReverse)
 
 
-if __name__ == '__main__':
-    wpilib.run(MyRobot)
+            
+if __name__ == "__main__":
+    wpilib.run(MyRobot) #runs the code!
